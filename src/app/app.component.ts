5ae430a54614a952479a47b2e553742e8411ce8b
Shafi Rayhan <rayhanshafi7@gmail.com>
2021-04-21 15:25:36 +0600

    angular drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'drag-and-drop-list';
+  list = [
+    'List Item 1',
+    'List Item 2',
+    'List Item 3',
+    'List Item 4',
+    'List Item 5',
+    'List Item 6',
+    'List Item 7',
+    'List Item 8',
+    'List Item 9',
+    'List Item 10',
+  ];
+
+  dropHandler(event) {
+    console.log(event);
+    const { previousIndex: from, currentIndex: to } = event;
+    moveItemInArray(this.list, from, to);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, DragDropModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
